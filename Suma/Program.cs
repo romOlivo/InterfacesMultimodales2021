05d28b7a51bbf231f571f96a604134a15554a870
romOlivo <al341918@sg.uji.es>
2021-04-28 16:00:53 +0200

    Operation + done

---
+++ Suma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Introduzca un número");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca otro número");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("El resultado de la suma es {0}", a+b);
+        }
+    }
+}
