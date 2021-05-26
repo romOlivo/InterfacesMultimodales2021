d0abce7692f339a834e4e33de72bcd49e7263400
romOlivo <al341918@sg.uji.es>
2021-05-26 17:28:33 +0200

    Animations

--- DemoCanvas/MainWindow.xaml.cs
+++ DemoCanvas/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -73,6 +74,40 @@ namespace DemoCanvas
             MyCanvas.Children.Add(slider);
             Binding binding = new Binding("Value") { Source = slider };
             BindingOperations.SetBinding(r, Canvas.LeftProperty, binding);
+
+            Slider slider3 = new Slider() { Minimum = 0, Maximum = 360, Width = 200 }; 
+            TransformGroup tg = new TransformGroup(); 
+            RotateTransform rt = new RotateTransform() { CenterX = r.Width / 2, CenterY = r.Height / 2 }; 
+            tg.Children.Add(rt); r.RenderTransform = tg; 
+            slider3.Height = 200;
+            Canvas.SetTop(slider3, 250);
+            MyCanvas.Children.Add(slider3); 
+            Binding binding3 = new Binding("Value"); 
+            binding3.Source = slider3; 
+            BindingOperations.SetBinding(rt, RotateTransform.AngleProperty, binding3);
+
+            Rectangle r2 = new Rectangle
+            {
+                Width = 200,
+                Height = 150,
+                Stroke = Brushes.Black,
+                StrokeThickness = 3,
+                Fill = Brushes.Orange
+            };
+            Canvas.SetTop(r2, 300);
+            Canvas.SetLeft(r2, 500);
+            MyCanvas.Children.Add(r2);
+
+            TransformGroup tg2 = new TransformGroup(); 
+            RotateTransform rt2 = new RotateTransform() { CenterX = r2.Width / 2, CenterY = r2.Height / 2 };
+            tg2.Children.Add(rt2); 
+            r2.RenderTransform = tg2; 
+            DoubleAnimation an = new DoubleAnimation(); 
+            an.Duration = new Duration(TimeSpan.FromSeconds(1)); 
+            an.From = 0; an.To = 360; 
+            an.FillBehavior = FillBehavior.Stop; 
+            an.RepeatBehavior = RepeatBehavior.Forever; 
+            rt2.BeginAnimation(RotateTransform.AngleProperty, an);
         }
 
         private void R_MouseDown(object sender, MouseButtonEventArgs e)
