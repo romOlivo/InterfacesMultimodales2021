60272a4a821079f35723b10cc40057cc8a122ab1
romOlivo <al341918@sg.uji.es>
2021-06-02 16:37:58 +0200

    Snake Skeleton

---
+++ Entrega1Snake/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Entrega1Snake
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly int N_BOXS_ROW = 45;
+        readonly int N_BOXS_COL = 45;
+        public MainWindow()
+        {
+            Loaded += MainWindow_Loaded;
+            InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            DrawInitialSnake();
+        }
+
+        private void DrawInitialSnake()
+        {
+            var elipse = new Ellipse()
+            {
+                Width = MyCanvas.Width / N_BOXS_COL,
+                Height = MyCanvas.Height / N_BOXS_ROW,
+                Stroke = Brushes.Black,
+                Fill = Brushes.Orange
+            };
+            Canvas.SetTop(elipse, 0);
+            Canvas.SetLeft(elipse, 0);
+            MyCanvas.Children.Add(elipse);
+        }
+    }
+}
