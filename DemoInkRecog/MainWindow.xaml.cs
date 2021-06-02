ef1a5a278ebb703dc8e225a845648495f5792d3c
romOlivo <al341918@sg.uji.es>
2021-06-02 17:57:19 +0200

    Calculadora Skeleton

---
+++ DemoInkRecog/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Ink;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoInkRecog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                myInkCanvas.Strokes.Save(ms);
+                var ink = new Ink();
+                ink.Load(ms.ToArray());
+                using (RecognizerContext context = new RecognizerContext())
+                {
+                    if (ink.Strokes.Count > 0)
+                    {
+                        context.Strokes = ink.Strokes;
+                        RecognitionStatus status;
+                        var result = context.Recognize(out status);
+                        if (status == RecognitionStatus.NoError)
+                            myLabel.Content = result.TopString;
+                        else
+                            MessageBox.Show("Recognition failed");
+                    }
+                    else
+                        MessageBox.Show("No stroke detected");
+                }
+            }
+        }
+    }
+}
---
+++ Entrega2Calculadora/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Entrega2Calculadora
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        DispatcherTimer timer;
+        public MainWindow()
+        {
+            Loaded += MainWindow_Loaded;
+            InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            myInkCanvas.MouseUp += MyInkCanvas_MouseUp;
+            myInkCanvas.MouseMove += MyInkCanvas_MouseMove;
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(2000)
+            };
+            timer.Tick += Timer_Tick;
+        }
+
+        private void MyInkCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            myLabel.Content = "Drawed";
+            timer.Stop();
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            myLabel.Content = "Recognized";
+        }
+
+        private void MyInkCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            myInkCanvas.Strokes.Clear();
+            myLabel.Content = "-";
+        }
+    }
+}
