867968ecd3892a9a08d6f244c5e8d257db1c5670
romOlivo <al341918@sg.uji.es>
2021-06-02 17:02:04 +0200

    DemoInkCanvas Done

---
+++ DemoInkCanvas/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoInkCanvas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DrawingAttributes da = new DrawingAttributes();
+        public MainWindow()
+        {
+            Loaded += MainWindow_Loaded;
+            InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            wSlider.Minimum = DrawingAttributes.MinWidth;
+            wSlider.Maximum = 10;
+            hSlider.Minimum = DrawingAttributes.MinHeight;
+            hSlider.Maximum = 10;
+            wSlider.Value = 1.0;
+            hSlider.Value = 1.0;
+            CanvasDeTinta.DefaultDrawingAttributes = da;
+        }
+
+        private void wSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) { da.Width = wSlider.Value; }
+
+        private void hSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) { da.Height = hSlider.Value; }
+        
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch ((e.Source as ComboBox).SelectedIndex)
+            {
+                case 0:
+                    da.Color = Colors.Black;
+                    break;
+                case 1:
+                    da.Color = Colors.Red;
+                    break;
+                case 2:
+                    da.Color = Colors.Blue;
+                    break;
+            }
+        }
+
+    }
+}
