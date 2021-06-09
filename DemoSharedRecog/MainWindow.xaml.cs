4a412c66451322530a8992bd212443746d779cb1
romOlivo <al341918@sg.uji.es>
2021-06-09 16:13:05 +0200

    DemoSharedRecog Done

---
+++ DemoSharedRecog/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoSharedRecog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        SpeechRecognizer sharedRecognizer = new SpeechRecognizer() { Enabled = true}; 
+        Grammar grammar= new DictationGrammar();
+        public MainWindow()
+        {
+            Loaded += MainWindow_Loaded;
+            InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            sharedRecognizer.LoadGrammar(grammar); 
+            sharedRecognizer.SpeechRecognized += SpeechRecognized; 
+            sharedRecognizer.SpeechRecognitionRejected += SpeechRecognitionRejected; 
+            sharedRecognizer.SpeechDetected += SpeechDetected;
+        }
+
+        void SpeechDetected(object sender, SpeechDetectedEventArgs e) { labelTextoReconocido.Content = "<Voz detectada>"; labelProbabilidad.Content = ""; }
+        void SpeechRecognitionRejected(object s, SpeechRecognitionRejectedEventArgs e) { labelTextoReconocido.Content = "<No le he oído bien. Repita por favor>"; labelProbabilidad.Content = ""; }
+        void SpeechRecognized(object sender, SpeechRecognizedEventArgs e) { labelTextoReconocido.Content = e.Result.Text; labelProbabilidad.Content = e.Result.Confidence.ToString(); }
+
+    }
+}
