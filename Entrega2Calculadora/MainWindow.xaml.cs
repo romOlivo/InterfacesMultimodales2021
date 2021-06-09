1073a5d604bbe6806d06808dd6254166e72a8327
romOlivo <al341918@uji.es>
2021-06-09 02:16:22 +0200

    Entrega2 Continue Done

--- Entrega2Calculadora/MainWindow.xaml.cs
+++ Entrega2Calculadora/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Microsoft.Ink;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +28,9 @@ namespace Entrega2Calculadora
     {
 
         DispatcherTimer timer;
+
+        private Boolean isDrawing;
+        private string[] delimiters = { "=" };
         public MainWindow()
         {
             Loaded += MainWindow_Loaded;
@@ -31,36 +39,87 @@ namespace Entrega2Calculadora
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            myInkCanvas.MouseUp += MyInkCanvas_MouseUp;
-            myInkCanvas.MouseMove += MyInkCanvas_MouseMove;
+            myInkCanvas.PreviewMouseUp += MyInkCanvas_MouseUp;
+            myInkCanvas.PreviewMouseMove += MyInkCanvas_MouseMove;
+            myInkCanvas.PreviewMouseDown += MyInkCanvas_MouseDown;
             timer = new DispatcherTimer
             {
                 Interval = TimeSpan.FromMilliseconds(2000)
             };
             timer.Tick += Timer_Tick;
+            isDrawing = false;
+        }
+
+        #region Eventos de Ratón
+
+        private void MyInkCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            isDrawing = false;
+        }
+
+        private void MyInkCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            isDrawing = true;
         }
 
         private void MyInkCanvas_MouseMove(object sender, MouseEventArgs e)
         {
-            myLabel.Content = "Drawed";
+            if (!isDrawing) return;
             timer.Stop();
             timer.Start();
         }
 
+        #endregion
+
+        #region Manejo de tinta
         private void Timer_Tick(object sender, EventArgs e)
         {
-            myLabel.Content = "Recognized";
+            using (MemoryStream ms = new MemoryStream())
+            {
+                myInkCanvas.Strokes.Save(ms);
+                var ink = new Ink();
+                ink.Load(ms.ToArray());
+                using (RecognizerContext context = new RecognizerContext())
+                {
+                    if (ink.Strokes.Count > 0)
+                    {
+                        context.Strokes = ink.Strokes;
+                        RecognitionStatus status;
+                        var result = context.Recognize(out status);
+                        if (status == RecognitionStatus.NoError)
+                        {
+                            Console.WriteLine(result.TopString.Last());
+                            if (result.TopString.Length > 0 &&
+                                Array.Exists(delimiters, elem => elem == $"{result.TopString.Last()}"))
+                                showResult(result.TopString.Substring(0, result.TopString.Length - 1));
+                            else
+                                myLabel.Content = result.TopString;
+                        }
+                        else
+                            MessageBox.Show("Recognition failed");
+                    }
+                    else
+                        MessageBox.Show("No stroke detected");
+                }
+            }
         }
 
-        private void MyInkCanvas_MouseUp(object sender, MouseButtonEventArgs e)
+        private void showResult(string s)
         {
-            
+            DataTable dt = new DataTable();
+            var sp = s.Replace("x", "*");
+            var v = dt.Compute(sp, "");
+            myLabel.Content = $"{s}= {v}";
+            timer.Stop();
         }
+
+        #endregion
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             myInkCanvas.Strokes.Clear();
             myLabel.Content = "-";
+            timer.Stop();
         }
     }
 }
