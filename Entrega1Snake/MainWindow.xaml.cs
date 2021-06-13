d751fa0d1949345bd63d80461d17b561d15e13b4
romOlivo <al341918@uji.es>
2021-06-13 23:36:22 +0200

    Entrega1 Movement

--- Entrega1Snake/MainWindow.xaml.cs
+++ Entrega1Snake/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Entrega1Snake
 {
@@ -22,6 +23,16 @@ namespace Entrega1Snake
     {
         readonly int N_BOXS_ROW = 45;
         readonly int N_BOXS_COL = 45;
+        int snakeColumn;
+        int snakeRow;
+
+        List<int[]> directions;
+        int direction;
+        int LEFT = -1;
+        int RIGHT = 1;
+
+        List<Ellipse> snake;
+        DispatcherTimer timer;
         public MainWindow()
         {
             Loaded += MainWindow_Loaded;
@@ -30,21 +41,72 @@ namespace Entrega1Snake
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
+            snake = new List<Ellipse>();
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(500)
+            };
+            timer.Tick += Timer_Tick;
+
+            directions = new List<int[]>();
+            directions.Add(new int[] { 0, 1 });
+            directions.Add(new int[] { 1, 0 });
+            directions.Add(new int[] { 0, -1 });
+            directions.Add(new int[] { -1, 0 });
+
+            direction = 0;
+
             DrawInitialSnake();
         }
 
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            updateSnake();
+        }
+
+        #region Serpiente
+
         private void DrawInitialSnake()
+        {
+            snakeColumn = 0;
+            snakeRow = 0;
+            snake.Add(drawNewEllipse(0, 0));
+            timer.Start();
+        }
+
+        private void updateSnake()
+        {
+            Console.WriteLine("Updating");
+            var head = snake.Last();
+            snake.RemoveAt(snake.Count - 1);
+            snake.Add(head);
+            Canvas.SetTop(head, MyCanvas.Width / N_BOXS_COL * snakeRow + 1);
+            Canvas.SetLeft(head, MyCanvas.Height / N_BOXS_ROW * snakeColumn + 1);
+
+
+            snakeRow = snakeRow + directions[direction][0];
+            snakeColumn = snakeColumn + directions[direction][1];
+        }
+
+        #endregion
+
+        #region Dibujo
+        private Ellipse drawNewEllipse(int i, int j)
         {
             var elipse = new Ellipse()
             {
-                Width = MyCanvas.Width / N_BOXS_COL,
-                Height = MyCanvas.Height / N_BOXS_ROW,
+                Width = MyCanvas.Width / N_BOXS_COL - 2,
+                Height = MyCanvas.Height / N_BOXS_ROW - 2,
                 Stroke = Brushes.Black,
                 Fill = Brushes.Orange
             };
-            Canvas.SetTop(elipse, 0);
-            Canvas.SetLeft(elipse, 0);
+            Canvas.SetTop(elipse, MyCanvas.Width / N_BOXS_COL * i + 1);
+            Canvas.SetLeft(elipse, MyCanvas.Height / N_BOXS_ROW * j + 1);
             MyCanvas.Children.Add(elipse);
+            return elipse;
         }
+
+        #endregion
+
     }
 }
