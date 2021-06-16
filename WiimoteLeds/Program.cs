6036d7cd515b11ee4b1fd4ba3f3a2f3e2400a608
Vicente López Oliva <al341918@sg.uji.es>
2021-06-16 16:21:01 +0200

    Wiimote Leds Done

---
+++ WiimoteLeds/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WiimoteLib;
+
+namespace WiimoteLeds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var wm = new Wiimote();
+
+            wm.Connect();
+
+            wm.SetLEDs(3);
+
+        }
+    }
+}
